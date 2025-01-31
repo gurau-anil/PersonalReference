b4913b10a6bfe950024dc73f0d78224783681707
gurau-anil <gurau@gmail.com>
2025-01-30 23:02:58 -0700

    minor refactor

--- HttpClientAndHttpClientFactory/Controllers/HttpClientTestController.cs
+++ HttpClientAndHttpClientFactory/Controllers/HttpClientTestController.cs
@@ -12,27 +12,32 @@ namespace HttpClientAndHttpClientFactory.Controllers
         private readonly ILogger<HttpClientTestController> _logger;
         private readonly ApiSettings _apiSettings;
         private static readonly HttpClient client = new HttpClient();
+
+        private readonly string apiUrl;
+        private readonly string apiKey;
         public HttpClientTestController(ILogger<HttpClientTestController> logger, IOptions<ApiSettings> apiSettings)
         {
             _logger = logger;
             _apiSettings = apiSettings.Value;
 
+
+            //Just a Mock Api
+            apiUrl = $"{_apiSettings.BaseUrl}/posts";
+            apiKey = _apiSettings.ApiKey;
         }
 
         [HttpGet(Name = "get-data")]
         public async Task<IActionResult> GetData()
         {
-            string apiUrl = $"{_apiSettings.BaseUrl}/posts";
-            string apiKey = _apiSettings.ApiKey;
 
-            
-            client.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiKey}");
+            PrepareHttpClient(client);
 
             try
             {
                 HttpResponseMessage response = await client.GetAsync(apiUrl);
                 response.EnsureSuccessStatusCode();
 
+                //Reading Raw Response Data
                 string responseData = await response.Content.ReadAsStringAsync();
                 return Ok(responseData);
             }
@@ -40,6 +45,17 @@ namespace HttpClientAndHttpClientFactory.Controllers
             {
                 return StatusCode(500, $"Request error: {e.Message}");
             }
+        }
+
+        private void PrepareHttpClient(HttpClient client)
+        {
+            AddRequestHeaders(client);
+        }
+
+        private void AddRequestHeaders(HttpClient client)
+        {
+            //adding custom Headers
+            client.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiKey}");
         }
     }
 }
