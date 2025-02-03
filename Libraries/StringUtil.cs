6de39f096fde022580792e8a64811d246c341adc
gurau-anil <gurau@gmail.com>
2025-02-03 00:24:55 -0700

    String Utils - Generate random strings

---
+++ Libraries/StringUtil.cs
@@ -0,0 +1,22 @@
+﻿
+namespace UtilBox
+{
+    public class StringUtil
+    {
+        public string GenerateRandomString(int length, bool includeDigits = true, bool includeSpecialChars = false)
+        {
+            const string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+            const string digits = "0123456789";
+            const string specialChars = "!@#$%^&*()_+-=[]{}|;:,.<>?";
+
+            string chars = letters;
+            if (includeDigits) chars += digits;
+            if (includeSpecialChars) chars += specialChars;
+
+            Random rand = new Random();
+            return new string(Enumerable.Range(0, length).Select(_ => chars[rand.Next(chars.Length)]).ToArray());
+        }
+    }
+
+
+}
