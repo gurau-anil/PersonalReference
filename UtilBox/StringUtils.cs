badfbd160b49637bd3c0cc0235f4a48813396104
gurau-anil <gurau@gmail.com>
2025-02-03 18:54:40 -0700

    String Helper updated and extension methods added

--- UtilBox/StringUtils.cs
+++ UtilBox/StringUtils.cs
@@ -1,11 +1,11 @@
 ﻿
 using System.Text.RegularExpressions;
 
-namespace UtilBox
+namespace UtilBox.StringUtils
 {
-    public class StringUtil
+    public static class StringHelper
     {
-        public string GenerateRandomString(int length, bool includeDigits = true, bool includeSpecialChars = false)
+        public static string GenerateRandomString(int length, bool includeDigits = true, bool includeSpecialChars = false)
         {
             const string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
             const string digits = "0123456789";
@@ -19,29 +19,36 @@ namespace UtilBox
             return new string(Enumerable.Range(0, length).Select(_ => chars[rand.Next(chars.Length)]).ToArray());
         }
 
-        public int WordCount(string input)
+    }
+
+
+    public static class StringExtension
+    {
+        public static int WordCount(this string input)
         {
             var matches = Regex.Matches(input, @"\S+");
             return matches.Count;
         }
 
-        public string RemoveWhiteSpaces(string input)
+        public static string RemoveWhiteSpaces(this string input)
         {
             return Regex.Replace(input, @"\s+", " ");
         }
 
-        public static string CapitalizeFirstLetter(string input)
+        public static string CapitalizeFirstLetter(this string input)
         {
-            return string.IsNullOrEmpty(input)? input : char.ToUpper(input[0]) + input.Substring(1);
+            return string.IsNullOrEmpty(input) ? input : char.ToUpper(input[0]) + input.Substring(1);
         }
 
-        public string ToSentenceCase(string input)
+        public static string ToSentenceCase(this string input)
         {
             return string.IsNullOrEmpty(input) ? input : input.Substring(0, 1).ToUpper() + input.Substring(1).ToLower();
         }
 
+        public static bool CompareWith(this string input, string compareWith, bool ignoreCase = false)
+        {
+            return ignoreCase ? string.Equals(input, compareWith, StringComparison.OrdinalIgnoreCase) : string.Equals(input, compareWith);
+        }
 
     }
-
-
 }
