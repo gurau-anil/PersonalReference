84decad9dbd27554a3a8bf83ab3d98b13478e3e9
gurau-anil <gurau.anil@gmail.com>
2025-04-02 12:03:08 -0700

    checking utitlity

---
+++ LibraryTestProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using UtilBox.StringUtils;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string name = "Anil gurau";
+
+        string anotherName = "anil gurau";
+
+        string phoneNumber = "5205263540";
+        string cardNumber = "3450 2356 2356 4598";
+        Console.WriteLine(StringHelper.GenerateRandomPassword(10));
+        Console.WriteLine(StringHelper.GenerateRandomString(10));
+        Console.WriteLine(StringHelper.GenerateUniqueIdentifier());
+        Console.WriteLine(phoneNumber.FormatAndMaskPhoneNumber());
+        Console.WriteLine(cardNumber.MaskCreditCard());
+        Console.WriteLine(name.CompareWith(anotherName, ignoreCase: true));
+    }
+}
